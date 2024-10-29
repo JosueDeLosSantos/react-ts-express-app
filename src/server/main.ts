3ab3e67cb798ac991e8190745b21b9bc434ebaef
joshcoder2000 <jdd1993@hotmail.com>
2024-10-29 18:15:18 -0300

    Adds additional configuration

--- src/server/main.ts
+++ src/server/main.ts
@@ -10,6 +10,8 @@ process.loadEnvFile();
  */
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // adds cors middleware that accepts requests from any origin
 app.use(cors());
